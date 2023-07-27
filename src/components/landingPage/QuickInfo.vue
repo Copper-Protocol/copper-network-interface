af5502b5e4165530bd2cc251cb00a695b6c8d889
Fraktal DeFi DAO <fraktaldefi.dao@ud.me>
2023-07-26 20:49:56 -0500

    ...

--- src/components/landingPage/QuickInfo.vue
+++ src/components/landingPage/QuickInfo.vue
@@ -1,6 +1,9 @@
 <script setup>
 import BaseContainer from "@/components/base/Container.vue"
-import paceHolder1 from "@/assets/images/placeholder1.jpg"
+import placeHolder1 from "@/assets/images/placeholder1.jpg"
+import placeHolder3 from "@/assets/images/placeholder3.jpg"
+import vidPlaceHolder1 from "@/assets/images/vid-placeholder.jpg"
+// images/placeholder3.jpg
 </script>
 
 <template>
@@ -10,7 +13,7 @@ import paceHolder1 from "@/assets/images/placeholder1.jpg"
       <div class="p-4">
         <h2 class="text-3xl font-bold">About Copper Network</h2>
         <div class="">
-          <div class=""><img :src="paceHolder1" width="503" alt="" class="screenshot"></div>
+          <div class=""><img :src="placeHolder1" width="503" alt="" class="screenshot"></div>
           <div class="">
             <p class="">In an increasingly digital world, traditional centralized systems consistently pose significant obstacles to progress with outdated frameworks and intermediaries who stifle innovation, limit transparency, and impede the growth of marginalized communities seeking to shape their own destiny.  Businesses struggle to build resilient and efficient ecosystems, supply chains face inefficiencies and lack traceability, and payment networks remain burdened by lengthy processes and high fees. In addition, the alarming reality of a widening racial wealth gap presents a pressing concern that demands immediate attention. Despite efforts to address systemic inequities, a clear path to effectively tackle this issue remains elusive. Black Americans, whose buying power contributes a staggering $2 trillion to the GDP, have not been met with the reciprocity and economic opportunities they deserve. The absence of a comprehensive solution perpetuates this inequality, hindering the advancement and prosperity of marginalized communities. It&#x27;s clear that a transformative solution is needed to break free from these constraints and unlock the true potential of collaboration and sovereignty.</p>
           </div>
@@ -22,12 +25,12 @@ import paceHolder1 from "@/assets/images/placeholder1.jpg"
         <h2 class="">Why Copper Network?</h2>
         <p class="">Imagine a world where value, payments, and money is moving as fast as information.  Through the power of blockchain, we provide the tools and infrastructure to onboard enterprises to web3, enable seamless collaboration, enhanced transparency, and reduced friction across industries. By aligning incentives, leveraging decentralized networks, and using smart contract capabilities, we usher in a new era of trust and efficiency where participants can benefit from community networks and governance. Together, we build a future where businesses thrive, supply chains operate seamlessly, and payment networks empower individuals to transact with ease.</p>
       </div>
-      <div class=""><img src="images/vid-placeholder.jpg" width="502" alt="" class="screenshot">
+      <div class=""><img :src="vidPlaceHolder1" width="502" alt="" class="screenshot">
         <p class="">Double click an image to replace it.</p>
       </div>
     </div>
     <div class="p-4">
-      <div class=""><img src="images/placeholder3.jpg" width="240" alt="" class="screenshot">
+      <div class=""><img :src="placeHolder3" width="240" alt="" class="screenshot">
         <p class="">You can also double click an image to change its size.</p>
       </div>
       <div class="">
