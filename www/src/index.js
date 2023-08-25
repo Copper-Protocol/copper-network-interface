b270cabdae4204d522a494cc644c6f4f0560e0db
Krypto Kajun <kryptokajun@proton.me>
2023-08-25 02:33:20 -0500

    ...

--- www/src/index.js
+++ www/src/index.js
@@ -29,12 +29,13 @@ app.use(express.static('dist'))
 
 // custom 404
 app.use((req, res, next) => {
-    res.status(404).send("Sorry can't find that!")
-  })
-  
-  // custom error handler
-  app.use((err, req, res, next) => {
+res.status(404).send("Sorry can't find that!")
+})
+
+// custom error handler
+app.use((err, req, res, next) => {
     console.error(err.stack)
     res.status(500).send('Something broke!')
-  })
+})
+
 app.listen(port, host, () => console.log(`Copper Web Server Running on ${host}:${port}`))
