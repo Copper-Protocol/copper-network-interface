e21f4fdc2cb3f61f3b90e05ef858c7b4fb696c49
Krypto Kajun <kryptokajun@proton.me>
2023-08-30 15:49:10 -0500

    fixed Dockerfile...

--- www/src/index.js
+++ www/src/index.js
@@ -29,14 +29,14 @@ app.use('/', express.static(process.cwd() + 'dist'))
 // last app.use calls right before app.listen():
 
 // custom 404
-app.use((req, res, next) => {
-    res.status(404).send("Sorry can't find that!")
-})
+// app.use((req, res, next) => {
+//     res.status(404).send("Sorry can't find that! => ", process.cwd())
+// })
 
-// custom error handler
-app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-})
+// // custom error handler
+// app.use((err, req, res, next) => {
+//     console.error(err.stack)
+//     res.status(500).send('Something broke!')
+// })
 
 app.listen(port, host, () => console.log(`Copper Web Server Running on ${host}:${port}`))
