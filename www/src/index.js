90464dfcac30f601c6ea30791c481a2b16206672
Krypto Kajun <kryptokajun@proton.me>
2023-08-30 17:58:09 -0500

    fixed Dockerfile...

--- www/src/index.js
+++ www/src/index.js
@@ -28,7 +28,7 @@ console.log(`-->`, process.cwd() + 'dist')
 app.use('/', express.static('dist'))
 // last app.use calls right before app.listen():
 
-custom 404
+// custom 404
 app.use((req, res, next) => {
     res.status(404).send("Sorry can't find that! => ", process.cwd())
 })
